8f677f292e0ae7c2e72dd84f055184623224a0c6
BSantaCruzSilva <89657723+BSantaCruzSilva@users.noreply.github.com>
2021-11-06 17:36:15 -0300

    Cambios clase Reserva

--- TRABAJOOBLIGATORIO/src/Clases/Reserva.java
+++ TRABAJOOBLIGATORIO/src/Clases/Reserva.java
@@ -1,7 +1,7 @@
 
 package Clases;
-
 import Estructuras.Lista;
+
 
 public class Reserva extends Lista {
     private int cliente;
@@ -64,4 +64,97 @@ public class Reserva extends Lista {
     public void setFecha(String fecha) {
         this.fecha = fecha;
     } 
+    
+    //Metodos para sobrecarga 
+    
+    
+    public boolean esVacia() {
+        return this.primero == null;
+    }
+    
+    public boolean puedoInsertar() { //CONSULTAR NECESIDAD DE MÉTODO, SE REALIZA CON EJEMPLARES DE LIBRO
+        return true; 
+    }
+   
+    public boolean agregarFinal(Reserva n) {
+        if (puedoInsertar()) {
+            this.actual++;
+            //NodoLista nuevo= new NodoLista(n);
+            if (this.esVacia()) {
+                this.primero = n;
+            } else {
+                Reserva aux = this.primero;
+                while (aux.getSiguiente() != null) {
+                    aux = aux.getSiguiente();
+                }
+                Reserva nuevo = new Reserva(n.cliente, n.numero, n.fecha);
+                aux.setSiguiente(nuevo);
+                this.ultimo = nuevo;
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    public Reserva obtenerElemento(Reserva n) {
+        if (!this.esVacia()) {
+            if (this.primero.getDato() == n) {
+                return primero;
+            }
+            if (this.ultimo.getDato() == n) {
+                return ultimo;
+            } else {
+                Reserva aux = this.getPrimero();
+                while (aux.getSiguiente() != null) {
+                    if (aux.getDato() == n) {
+                        return aux;
+                    }
+                    aux = aux.getSig();
+                }
+            }
+        }
+        return null;
+    }
+    
+    public boolean eliminarElemento(Reserva n) {
+        //si no es null
+        if (!esVacia()) {
+            if (n == getPrimero()) {
+                borrarInicio();
+                return true;
+
+            } else if (n == getUltimo()) {
+                borrarFin();
+                return true;
+
+            } else {
+
+                Reserva aux = this.obtenerElemento(n);
+
+                if (aux.getSiguiente() != getUltimo()) {
+                    aux.getSiguiente() = aux.getSiguiente().getSiguiente();
+                    return true;
+
+                }
+            }
+
+        }
+        return false;
+    }
+    
+     public void mostrarREC(Reserva l) {
+        if (l != null) {
+            System.out.println(l.getDato());
+            mostrarREC(l.getSiguiente());
+        }
+    }
+     
+     public void vaciar() {
+        this.actual = 0;
+        //en java alcanza con apuntar inicio y fin a null
+        //inicio=fin=null;
+        while (primero != null) {
+            borrarInicio();
+        }
+    }
 }
