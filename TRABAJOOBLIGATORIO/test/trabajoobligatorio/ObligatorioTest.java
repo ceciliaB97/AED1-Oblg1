11b6990bb34b6b80bb0a167a8a676015e4678dfa
BSantaCruzSilva <89657723+BSantaCruzSilva@users.noreply.github.com>
2021-11-17 19:25:59 -0300

    Arreglar reserva

--- TRABAJOOBLIGATORIO/test/trabajoobligatorio/ObligatorioTest.java
+++ TRABAJOOBLIGATORIO/test/trabajoobligatorio/ObligatorioTest.java
@@ -1,259 +1,258 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package trabajoobligatorio;
 
+import Clases.*;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-/**
- *
- * @author Usuario
- */
 public class ObligatorioTest {
-    
+
     public ObligatorioTest() {
     }
 
-    /**
-     * Test of crearSistemaReservas method, of class Obligatorio.
-     */
+//    @Test
+//    public void testCrearSistemaReservas() {
+//        System.out.println("crearSistemaReservas");
+//        Obligatorio instance = new Obligatorio();
+//        Retorno.Resultado expResult = Retorno.Resultado.OK;
+//        Retorno.Resultado result = instance.crearSistemaReservas().resultado;
+//        assertEquals(expResult, result); 
+//    }   
+//    
+//    @Test
+//    public void testRegistrarBiblioteca() {
+//        System.out.println("registrarBiblioteca");        
+//        String Biblioteca = "Biblioteca";
+//        Obligatorio instance = new Obligatorio();
+//        Retorno.Resultado expResult = Retorno.Resultado.OK;
+//        Retorno.Resultado result = instance.registrarBiblioteca(Biblioteca).resultado;
+//        assertEquals(expResult, result);        
+//    }
+////
+//    
+//    @Test
+//    public void testEliminarBiblioteca() {
+//        System.out.println("eliminarBiblioteca");
+//        String Biblioteca = "Biblioteca";
+//        Obligatorio instance = new Obligatorio();
+//        Retorno.Resultado expResult = Retorno.Resultado.OK;
+//        Retorno.Resultado result = instance.eliminarBiblioteca(Biblioteca).resultado;
+//        assertEquals(expResult, result);
+//    }
+//
+//    
+//    @Test
+//    public void testRegistrarLibro() {
+//        System.out.println("registrarLibro");
+//        String titulo = "Libro";
+//        String editorial = "Editorial";
+//        String biblioteca = "Biblioteca";
+//        int ejemplares = 0;
+//        Obligatorio instance = new Obligatorio();
+//        Retorno.Resultado expResult = Retorno.Resultado.OK;
+//        Retorno.Resultado result = instance.registrarLibro(titulo, editorial, biblioteca, ejemplares).resultado;
+//        assertEquals(expResult, result);        
+//    }
+//
+//    
+//    @Test
+//    public void testEliminarLibro() {
+//        System.out.println("eliminarLibro");
+//        String titulo = "Libro";
+//        String editorial = "Editorial";
+//        String biblioteca = "Biblioteca";
+//        Obligatorio instance = new Obligatorio();
+//        Retorno.Resultado expResult = Retorno.Resultado.OK;
+//        Retorno.Resultado result = instance.eliminarLibro(titulo, editorial, biblioteca).resultado;
+//        assertEquals(expResult, result);
+//    }
+//
+//    
+//    @Test
+//    public void testRegistrarCalificacion() {
+//        System.out.println("RegistrarCalificacion");
+//        String titulo = "Libro";
+//        String editorial = "Editorial";
+//        int calificacion = 3;
+//        String biblioteca = "Biblioteca";
+//        String comentario = "Comentario";
+//        Obligatorio instance = new Obligatorio();
+//        Retorno.Resultado expResult = Retorno.Resultado.OK;
+//        Retorno.Resultado result = instance.RegistrarCalificacion(titulo, editorial, calificacion, biblioteca, comentario).resultado;
+//        assertEquals(expResult, result);
+//    }
+//
+//    @Test
+//    public void testRealizarReserva() {
+//        System.out.println("realizarReserva");
+//        int cliente = 1;
+//        int numero = 1;
+//        String biblioteca = "Biblioteca";
+//        String título = "Libro";
+//        String editorial = "Editorial";
+//        String fecha = "01/01/2021";
+//        Obligatorio instance = new Obligatorio();
+//        Retorno.Resultado expResult = Retorno.Resultado.OK;
+//        Retorno.Resultado result = instance.realizarReserva(cliente, numero, biblioteca, título, editorial, fecha).resultado;
+//        assertEquals(expResult, result);
+//    }
+//
+//    @Test
+//    public void testCancelarReserva() {
+//        System.out.println("cancelarReserva");
+//        int cliente = 1;
+//        int numero = 1;
+//        String biblioteca = "Biblioteca";
+//        Obligatorio instance = new Obligatorio();
+//        Retorno.Resultado expResult = Retorno.Resultado.OK;
+//        Retorno result = instance.cancelarReserva(cliente, numero, biblioteca);
+//        assertEquals(expResult, result);
+//    }    
+//
+//  
+//    @Test
+//    public void testListarLibros() {
+//        System.out.println("listarLibros");
+//        String biblioteca = "Biblioteca";
+//        Obligatorio instance = new Obligatorio();
+//        Retorno.Resultado expResult = Retorno.Resultado.OK;
+//        Retorno.Resultado result = instance.listarLibros(biblioteca).resultado;
+//        assertEquals(expResult, result);
+//    }
+//
+//    
+//    @Test
+//    public void testListarLibrosBiblioteca() {
+//        System.out.println("listarLibrosBiblioteca");
+//        String biblioteca = "Biblioteca";
+//        Obligatorio instance = new Obligatorio();
+//        Retorno.Resultado expResult = Retorno.Resultado.OK;
+//        Retorno.Resultado result = instance.listarLibrosBiblioteca(biblioteca).resultado;
+//        assertEquals(expResult, result);
+//    }
+//   
+//    @Test
+//    public void testListarBibliotecaRanking() {
+//        System.out.println("listarBibliotecaRanking");
+//        Obligatorio instance = new Obligatorio();
+//        Retorno expResult = null;
+//        Retorno result = instance.listarBibliotecaRanking();
+//        assertEquals(expResult, result);
+//    }
+//  
+//    @Test
+//    public void testListarComentarios() {
+//        System.out.println("listarComentarios");
+//        String biblioteca = "Biblioteca";
+//        Obligatorio instance = new Obligatorio();
+//        Retorno.Resultado expResult = Retorno.Resultado.OK;
+//        Retorno.Resultado result = instance.listarComentarios(biblioteca).resultado;
+//        assertEquals(expResult, result);
+//    }
+//
+//    @Test
+//    public void testListarEspera() {
+//        System.out.println("listarEspera");
+//        String titulo = "";
+//        String editorial = "";
+//        String biblioteca = "";
+//        Obligatorio instance = new Obligatorio();
+//        Retorno.Resultado expResult = Retorno.Resultado.OK;
+//        Retorno.Resultado result = instance.listarEspera(titulo, editorial, biblioteca).resultado;
+//        assertEquals(expResult, result);
+//    }
+//    
+//    @Test
+//    public void testMostrarReservasBiblioteca() {
+//        System.out.println("mostrarReservasBiblioteca");
+//        Obligatorio instance = new Obligatorio();
+//        Retorno.Resultado expResult = Retorno.Resultado.OK;
+//        Retorno.Resultado result = instance.mostrarReservasBiblioteca().resultado;
+//        assertEquals(expResult, result);
+//    }
+//    
+//    @Test
+//    public void testDestruirSistemaReservas() {
+//        System.out.println("destruirSistemaReservas");
+//        Obligatorio instance = new Obligatorio();
+//        Retorno.Resultado expResult = Retorno.Resultado.OK;
+//        Retorno.Resultado result = instance.destruirSistemaReservas().resultado;
+//        assertEquals(expResult, result);        
+//    }
     @Test
-    public void testCrearSistemaReservas() {
+    public void TestObligatorio() {
+
         System.out.println("crearSistemaReservas");
         Obligatorio instance = new Obligatorio();
-        Retorno expResult = null;
-        Retorno result = instance.crearSistemaReservas();
+        Retorno.Resultado expResult = Retorno.Resultado.OK;
+        Retorno.Resultado result = instance.crearSistemaReservas().resultado;
         assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
 
-    /**
-     * Test of destruirSistemaReservas method, of class Obligatorio.
-     */
-    @Test
-    public void testDestruirSistemaReservas() {
-        System.out.println("destruirSistemaReservas");
-        Obligatorio instance = new Obligatorio();
-        Retorno expResult = null;
-        Retorno result = instance.destruirSistemaReservas();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of registrarBiblioteca method, of class Obligatorio.
-     */
-    @Test
-    public void testRegistrarBiblioteca() {
         System.out.println("registrarBiblioteca");
-        String Biblioteca = "";
-        Obligatorio instance = new Obligatorio();
-        Retorno expResult = null;
-        Retorno result = instance.registrarBiblioteca(Biblioteca);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
+        String Biblioteca = "Biblioteca";
+        Retorno.Resultado result1 = instance.registrarBiblioteca(Biblioteca).resultado;
+        assertEquals(expResult, result1);
 
-    /**
-     * Test of eliminarBiblioteca method, of class Obligatorio.
-     */
-    @Test
-    public void testEliminarBiblioteca() {
-        System.out.println("eliminarBiblioteca");
-        String Biblioteca = "";
-        Obligatorio instance = new Obligatorio();
-        Retorno expResult = null;
-        Retorno result = instance.eliminarBiblioteca(Biblioteca);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of registrarLibro method, of class Obligatorio.
-     */
-    @Test
-    public void testRegistrarLibro() {
         System.out.println("registrarLibro");
-        String titulo = "";
-        String editorial = "";
-        String biblioteca = "";
+        String titulo = "Libro";
+        String editorial = "Editorial";
+        String biblioteca = "Biblioteca";
         int ejemplares = 0;
-        Obligatorio instance = new Obligatorio();
-        Retorno expResult = null;
-        Retorno result = instance.registrarLibro(titulo, editorial, biblioteca, ejemplares);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of eliminarLibro method, of class Obligatorio.
-     */
-    @Test
-    public void testEliminarLibro() {
-        System.out.println("eliminarLibro");
-        String titulo = "";
-        String editorial = "";
-        String biblioteca = "";
-        Obligatorio instance = new Obligatorio();
-        Retorno expResult = null;
-        Retorno result = instance.eliminarLibro(titulo, editorial, biblioteca);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of RegistrarCalificacion method, of class Obligatorio.
-     */
-    @Test
-    public void testRegistrarCalificacion() {
+        Retorno.Resultado result3 = instance.registrarLibro(titulo, editorial, biblioteca, ejemplares).resultado;
+        assertEquals(expResult, result3);
+        
         System.out.println("RegistrarCalificacion");
-        String titulo = "";
-        String editorial = "";
-        int calificacion = 0;
-        String biblioteca = "";
-        String comentario = "";
-        Obligatorio instance = new Obligatorio();
-        Retorno expResult = null;
-        Retorno result = instance.RegistrarCalificacion(titulo, editorial, calificacion, biblioteca, comentario);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
+        int calificacion = 3;
+        String comentario = "Comentario";
+        Retorno.Resultado result5 = instance.RegistrarCalificacion(titulo, editorial, calificacion, biblioteca, comentario).resultado;
+        assertEquals(expResult, result5);
 
-    /**
-     * Test of realizarReserva method, of class Obligatorio.
-     */
-    @Test
-    public void testRealizarReserva() {
         System.out.println("realizarReserva");
-        int cliente = 0;
-        int numero = 0;
-        String biblioteca = "";
-        String título = "";
-        String editorial = "";
-        String fecha = "";
-        Obligatorio instance = new Obligatorio();
-        Retorno expResult = null;
-        Retorno result = instance.realizarReserva(cliente, numero, biblioteca, título, editorial, fecha);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
+        int cliente = 1;
+        int numero = 1;
+        String fecha = "01/01/2021";
+        Retorno.Resultado result6 = instance.realizarReserva(cliente, numero, biblioteca, titulo, editorial, fecha).resultado;
+        assertEquals(expResult, result6);
 
-    /**
-     * Test of cancelarReserva method, of class Obligatorio.
-     */
-    @Test
-    public void testCancelarReserva() {
-        System.out.println("cancelarReserva");
-        int cliente = 0;
-        int numero = 0;
-        String biblioteca = "";
-        Obligatorio instance = new Obligatorio();
-        Retorno expResult = null;
-        Retorno result = instance.cancelarReserva(cliente, numero, biblioteca);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of listarLibros method, of class Obligatorio.
-     */
-    @Test
-    public void testListarLibros() {
         System.out.println("listarLibros");
-        String biblioteca = "";
-        Obligatorio instance = new Obligatorio();
-        Retorno expResult = null;
-        Retorno result = instance.listarLibros(biblioteca);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
+        Retorno.Resultado result8 = instance.listarLibros(biblioteca).resultado;
+        assertEquals(expResult, result8);
 
-    /**
-     * Test of listarLibrosBiblioteca method, of class Obligatorio.
-     */
-    @Test
-    public void testListarLibrosBiblioteca() {
         System.out.println("listarLibrosBiblioteca");
-        String biblioteca = "";
-        Obligatorio instance = new Obligatorio();
-        Retorno expResult = null;
-        Retorno result = instance.listarLibrosBiblioteca(biblioteca);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
+        Retorno.Resultado result9 = instance.listarLibrosBiblioteca(biblioteca).resultado;
+        assertEquals(expResult, result9);
 
-    /**
-     * Test of listarBibliotecaRanking method, of class Obligatorio.
-     */
-    @Test
-    public void testListarBibliotecaRanking() {
         System.out.println("listarBibliotecaRanking");
-        Obligatorio instance = new Obligatorio();
-        Retorno expResult = null;
-        Retorno result = instance.listarBibliotecaRanking();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
+        Retorno.Resultado result10 = instance.listarBibliotecaRanking().resultado;
+        assertEquals(expResult, result10);
 
-    /**
-     * Test of listarComentarios method, of class Obligatorio.
-     */
-    @Test
-    public void testListarComentarios() {
         System.out.println("listarComentarios");
-        String biblioteca = "";
-        Obligatorio instance = new Obligatorio();
-        Retorno expResult = null;
-        Retorno result = instance.listarComentarios(biblioteca);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
+        Retorno.Resultado result11 = instance.listarComentarios(biblioteca).resultado;
+        assertEquals(expResult, result11);
 
-    /**
-     * Test of listarEspera method, of class Obligatorio.
-     */
-    @Test
-    public void testListarEspera() {
         System.out.println("listarEspera");
-        String titulo = "";
-        String editorial = "";
-        String biblioteca = "";
-        Obligatorio instance = new Obligatorio();
-        Retorno expResult = null;
-        Retorno result = instance.listarEspera(titulo, editorial, biblioteca);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        Retorno.Resultado result12 = instance.listarEspera(titulo, editorial, biblioteca).resultado;
+        assertEquals(expResult, result12);
+
+        System.out.println("mostrarReservasBiblioteca");
+        Retorno.Resultado result13 = instance.mostrarReservasBiblioteca().resultado;
+        assertEquals(expResult, result13);
+
+        System.out.println("cancelarReserva");
+        Retorno.Resultado result7 = instance.cancelarReserva(cliente, numero, biblioteca).resultado;
+        assertEquals(expResult, result7);
+
+        System.out.println("eliminarLibro");
+        Retorno.Resultado result4 = instance.eliminarLibro(titulo, editorial, biblioteca).resultado;
+        assertEquals(expResult, result4);
+
+        System.out.println("eliminarBiblioteca");
+        Retorno.Resultado result2 = instance.eliminarBiblioteca(Biblioteca).resultado;
+        assertEquals(expResult, result2);
+
+        System.out.println("destruirSistemaReservas");
+        Retorno.Resultado result14 = instance.destruirSistemaReservas().resultado;
+        assertEquals(expResult, result14);
+
     }
 
-    /**
-     * Test of mostrarReservasBiblioteca method, of class Obligatorio.
-     */
-    @Test
-    public void testMostrarReservasBiblioteca() {
-        System.out.println("mostrarReservasBiblioteca");
-        Obligatorio instance = new Obligatorio();
-        Retorno expResult = null;
-        Retorno result = instance.mostrarReservasBiblioteca();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-    
 }
